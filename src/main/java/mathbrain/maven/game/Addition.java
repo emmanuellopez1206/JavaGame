602f56afab62bf164af683be0991665d40724900
Emmanuel Lopez <104656284+emmanuellopez1206@users.noreply.github.com>
2023-11-17 18:19:40 -0500

    Documentación clase suma

--- src/main/java/mathbrain/maven/game/Addition.java
+++ src/main/java/mathbrain/maven/game/Addition.java
@@ -1,9 +1,13 @@
 package mathbrain.maven.game;
-
+/*
+*Esta clase se usa para las operaciones matemáticas de Suma.
+*/
 public class Addition {
 	protected String problem;
 	protected int solution;
-	
+/*
+*Este método genera la operación matemática suma que se debe resolver.
+*/
 	public Addition() {
 		super(.5);
 		int number1 = (int)(Math.random() * 8) + 3;
@@ -11,11 +15,17 @@ public class Addition {
 		solution = number1 + number2;
 		problem = "" + number1 + "+" + number2;
 	}
-	
+/*
+*Este método obtiene la suma que se debe resolver.
+*@return Una cadena con la operación de suma.
+*/
 	public String getProblem() {
 		return problem;
 	}
-	
+/*
+*Este método obtiene el resultado de la operación matemática.
+*@return Un entero con el resultado de la suma.
+*/
 	public int getSolution() {
 		return solution;
 	}
